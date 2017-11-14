ab122c28a0b2606c09049087a7991d06746906a9
jack <e.kobets@talantarium.com>
2017-11-14 19:37:43 +0200

    Refactored.

--- src/RecursiveFileSearch/FileSearchGroovy.groovy
+++ src/RecursiveFileSearch/FileSearchGroovy.groovy
@@ -1,19 +1,17 @@
 package RecursiveFileSearch
 
-import java.lang.reflect.Array
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.stream.Collectors
-import java.util.stream.Stream
 
 /**
  * @Author Jack <e.kobets>
  */
-class FileSesrchGroovy {
+class FileSearchGroovy {
     static void main(String[] args) {
         File input = new File("src/data");
         File result = new File("src/result.txt");
-        result <<'DIRRECTORY;FILE;ACTION' + '\n';
+        result << 'DIRRECTORY;FILE;ACTION' + '\n';
 
         method(input, result);
     }
@@ -87,23 +85,20 @@ class FileSesrchGroovy {
 
     static void method(final File input, final File result) {
         if (input.isDirectory()) {
-//            result << '\n    ' + input.name + ':' + '\n';
             for (File file : input.listFiles().sort()) {
                 file.isDirectory() ? method(file, result) : writeResults(file, result);
             }
         } else {
-            for (File file : input.listFiles()) {
-                writeResults(input, result);
-            }
+            writeResults(input, result);
         }
     }
 
-    static void writeResults(final File file, final File result){
+    static void writeResults(final File file, final File result) {
         List<String> linesList = Files.lines(Paths.get(file.getPath())).collect(Collectors.toList());
         linesList.stream().skip(1).limit(1).forEach { line ->
             line.contains('actionType') ?
-                    result <<file.getPath().substring(0, file.getPath().lastIndexOf('/')) + ';' + file.name + ';'  + (line.substring(line.indexOf('actionType'))) + '\n' :
-                    result <<file.getPath().substring(0, file.getPath().lastIndexOf('/')) + ';' + file.name + ';'  + 'has a default action' + '\n'
+                    result << file.getPath().substring(0, file.getPath().lastIndexOf('/')) + ';' + file.name + ';' + (line.substring(line.indexOf('actionType'))) + '\n' :
+                    result << file.getPath().substring(0, file.getPath().lastIndexOf('/')) + ';' + file.name + ';' + 'has a default action' + '\n'
         };
     }
 }
